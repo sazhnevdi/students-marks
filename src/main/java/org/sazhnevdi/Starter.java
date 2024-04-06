4c12bc681f12d2850c2a60dc1dcd7e5c2b391627
dmitriy <dimon12@gmail.com>
2024-04-06 21:18:35 +0300

    initial commit

---
+++ src/main/java/org/sazhnevdi/Starter.java
@@ -0,0 +1,7 @@
+package org.sazhnevdi;
+
+public class Starter {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/org/sazhnevdi/StarterTest.java
@@ -0,0 +1,12 @@
+package org.sazhnevdi;
+
+import org.junit.jupiter.api.Test;
+
+class StarterTest {
+
+    @Test
+    public void test() {
+
+    }
+
+}
